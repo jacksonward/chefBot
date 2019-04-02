21402bbb8ff4fc7f8d9611fb06788dba265541e5
jackson <jackson.ward21@gmail.comgit config --global user.name jacksonward>
2019-04-02 00:10:20 -0500

    rm April Fools

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const got = require ('got')
 const BASE_URL = 'https://api.dineoncampus.com/v1/location/menu?site_id=5751fd2590975b60e04891fe&platform=0&'
 
 // Be sure to change botId for testing and production
-const botId = "134ad64e73ea10a1b5d038c45a"
+// const botId = "e91391980bd34125e9cb00406f" //testing
+const botId = "134ad64e73ea10a1b5d038c45a"	//production
 
 //Initialize global menu object
 let menu = {}
@@ -111,37 +112,6 @@ function sanitize(string) {
 	})
 
 	return message
-}
-
-// April Fools!
-function aprilFools(location, period) {
-
-	let grossStuff = ['Fried Worms', 'A normal burger but the cheese is moldy', 'A burger where everything is expired but the cheese is fine', 'Literal feces w/ rum sauce', 'Cold-ass Broccoli', 'Poppy-seed Beagle', 'Sheep\'s head', 'Pasta with toenail clippings', 'Garlic Mashed Potatoes, because heaven forbid we ever have regular mashed potatoes', 'Raw pork chops', 'Plastic-flavored Poptarts', 'Fermented Shark', 'Shell-in Sea Turtle', 'Miscellaneous Jelly Beans', 'Glob of dip-spit', 'Goat eye spread on toast', 'Barnacles', 'Curry Pancakes', 'Cod dipped in Seawater', '[Insert mediocre mexican food that no one really likes, but the other lines actually manage to be worse, so, what the hell]', 'Four Cheese Lasgna, but every cheese is cheddar', 'Bear Back Ribs', 'Canned Macaroni & Cheese', 'Mayonaise Sandwiches', 'Raw Oysters', 'Thousand Year Old Eggs', 'Mustard Pie']
-
-	function shuffle(array) {
-	  	var currentIndex = array.length, temporaryValue, randomIndex;
-	  	// While there remain elements to shuffle...
-	  	while (0 !== currentIndex) {
-
-		    // Pick a remaining element...
-		    randomIndex = Math.floor(Math.random() * currentIndex);
-		    currentIndex -= 1;
-
-		    // And swap it with the current element.
-		    temporaryValue = array[currentIndex];
-		    array[currentIndex] = array[randomIndex];
-		    array[randomIndex] = temporaryValue;
-	  	}
-  		return array;
-	}
-
-	let shuffledStuff = shuffle(grossStuff)
-	shuffledStuff = shuffledStuff.slice(0, 6)
-
-	let menuItemsString = shuffledStuff.join(', ')
-	let response = `${period} at ${location} will have the following: ${menuItemsString}.`
-
-	return response
 }
 
 function readMenu(location, period) {
@@ -315,10 +285,8 @@ app.post('/', function(req, res) {
 			}
 
 			// Declare response that bot will send to chat
-			// let response = readMenu(foundLoc, foundPeriod)
+			let response = readMenu(foundLoc, foundPeriod)
 
-			// April fools!!
-			let response = aprilFools(foundLoc, foundPeriod)
 			broadcast(response)
 		}
 	}
